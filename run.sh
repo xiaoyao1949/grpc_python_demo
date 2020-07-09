1d6b40602110cf357db816e537ba0aac58d1d0d4
liangrui <xiaoyao1949@outlook.com>
2020-07-09 15:12:34 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 virtualenv venv --python=python3
 source venv/bin/activate
+pip install -r requirements.txt
+
+rm *pb2*
+python -m grpc_tools.protoc -I ./ --python_out=./ --grpc_python_out=./ demo.proto
+
 python server.py &
 python client.py
